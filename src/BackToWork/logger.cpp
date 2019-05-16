41f5d199821979c13acc636ca2a4f2139771eb16
Taras Kushnir <kutaras@outlook.com>
2019-05-16 16:34:26 -0700

    Fix build

--- src/BackToWork/logger.cpp
+++ src/BackToWork/logger.cpp
@@ -10,7 +10,8 @@ Logger::~Logger() {
     std::lock_guard<std::mutex> guard(m_Mutex);
 
     auto t = std::time(nullptr);
-    auto tm = *std::localtime(&t);
+	tm tm;
+	localtime_s(&tm, &t);
     std::cout << std::put_time(&tm, "%H-%M-%S") << " - ";
 
     for (auto &s: m_LogLines) {
